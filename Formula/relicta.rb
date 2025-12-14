09cb6a30f447312578b3f88155b64de260cf84f8
Felix Geelhaar <felix@felixgeelhaar.de>
2025-12-14 20:42:40 +0100

    fix: use glob to find binary in extracted archive

--- Formula/relicta.rb
+++ Formula/relicta.rb
@@ -11,19 +11,11 @@ class Relicta < Formula
     on_intel do
       url "https://github.com/relicta-tech/relicta/releases/download/v2.0.0/relicta_Darwin_x86_64.tar.gz"
       sha256 "6989a4df22073ee592c88a5e566915d951c9e96435e1d5cb6be4cf9e92a3abbb"
-
-      def install
-        bin.install "relicta_Darwin_x86_64/relicta"
-      end
     end
 
     on_arm do
       url "https://github.com/relicta-tech/relicta/releases/download/v2.0.0/relicta_Darwin_aarch64.tar.gz"
       sha256 "c4f75612fdcbe000fa5b9fb5bb6b169b4eadd687bcf0dcafd50e5d8ab70b7abb"
-
-      def install
-        bin.install "relicta_Darwin_aarch64/relicta"
-      end
     end
   end
 
@@ -31,20 +23,18 @@ class Relicta < Formula
     on_intel do
       url "https://github.com/relicta-tech/relicta/releases/download/v2.0.0/relicta_Linux_x86_64.tar.gz"
       sha256 "5a36a84f72c8220d8410770fbeb414320f985b42a967337979567872372c1a4e"
-
-      def install
-        bin.install "relicta_Linux_x86_64/relicta"
-      end
     end
 
     on_arm do
       url "https://github.com/relicta-tech/relicta/releases/download/v2.0.0/relicta_Linux_aarch64.tar.gz"
       sha256 "67d4fb40105a2e5bc4a6c24d47879727a7ff879f1e8e70f5b53bb82a2d5f808f"
-
-      def install
-        bin.install "relicta_Linux_aarch64/relicta"
-      end
     end
+  end
+
+  def install
+    # Find the relicta binary in the extracted directory
+    binary = Dir["**/relicta"].first
+    bin.install binary => "relicta"
   end
 
   test do
