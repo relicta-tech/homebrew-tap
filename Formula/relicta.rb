c99053f722869adad4fbc85c007923f10d6a4517
Felix Geelhaar <felix@felixgeelhaar.de>
2025-12-17 20:17:04 +0100

    chore: update relicta to v2.3.0

--- Formula/relicta.rb
+++ Formula/relicta.rb
@@ -1,45 +1,37 @@
+# typed: false
+# frozen_string_literal: true
+
+# Homebrew formula for Relicta
 class Relicta < Formula
-  desc "AI-powered release management CLI"
+  desc "AI-powered release management for modern software teams"
   homepage "https://github.com/relicta-tech/relicta"
-  version "2.2.0"
+  version "2.3.0"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/relicta-tech/relicta/releases/download/v2.2.0/relicta_Darwin_aarch64.tar.gz"
-      sha256 "2e8f21f906e997ffd653a70d06e317a142a45e31d95c6432bc2ee15e4024cbcb"
+      url "https://github.com/relicta-tech/relicta/releases/download/v#{version}/relicta_Darwin_aarch64.tar.gz"
+      sha256 "12cef317b04571cdb2d0902dedc1b5a1a7550a4881e343be99cac70e3babf472"
     end
     on_intel do
-      url "https://github.com/relicta-tech/relicta/releases/download/v2.2.0/relicta_Darwin_x86_64.tar.gz"
-      sha256 "82bb9e098d4c9d242be1300e285cec49df3ab873c1ebda12a627d4236eda97e5"
+      url "https://github.com/relicta-tech/relicta/releases/download/v#{version}/relicta_Darwin_x86_64.tar.gz"
+      sha256 "cce6281388d485ee593989710581b55f316402333a66f644242f362cfdc3bf89"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/relicta-tech/relicta/releases/download/v2.2.0/relicta_Linux_aarch64.tar.gz"
-      sha256 "92a95630112b6b0e292aaf18a690a4b9ac2f4427285e8b423a20ff0fda8278a3"
+      url "https://github.com/relicta-tech/relicta/releases/download/v#{version}/relicta_Linux_aarch64.tar.gz"
+      sha256 "57db3067de854a76a893240971ce12f483d9532a45f8797ad3b14b5208d18480"
     end
     on_intel do
-      url "https://github.com/relicta-tech/relicta/releases/download/v2.2.0/relicta_Linux_x86_64.tar.gz"
-      sha256 "23fda479ed63cbf7c47a37992e1050dfa64ea56902514ab943247c51cb8d6d99"
+      url "https://github.com/relicta-tech/relicta/releases/download/v#{version}/relicta_Linux_x86_64.tar.gz"
+      sha256 "39632e74306658f6ffda25591d572468d1fbfd9dd842316efda6e3c34784a71f"
     end
   end
 
   def install
-    if OS.mac?
-      if Hardware::CPU.arm?
-        bin.install "relicta_Darwin_aarch64/relicta"
-      else
-        bin.install "relicta_Darwin_x86_64/relicta"
-      end
-    elsif OS.linux?
-      if Hardware::CPU.arm?
-        bin.install "relicta_Linux_aarch64/relicta"
-      else
-        bin.install "relicta_Linux_x86_64/relicta"
-      end
-    end
+    bin.install "relicta"
   end
 
   test do
