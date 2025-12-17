180dcbcccebb0009f257877a3682abf02d4db29a
Felix Geelhaar <felix@felixgeelhaar.de>
2025-12-17 16:16:54 +0100

    chore: update relicta to v2.2.0 (clean build)

--- Formula/relicta.rb
+++ Formula/relicta.rb
@@ -1,43 +1,48 @@
-# typed: false
-# frozen_string_literal: true
-
 class Relicta < Formula
-  desc "AI-powered release management for modern software teams"
+  desc "AI-powered release management CLI"
   homepage "https://github.com/relicta-tech/relicta"
   version "2.2.0"
   license "MIT"
 
   on_macos do
-    on_intel do
-      url "https://github.com/relicta-tech/relicta/releases/download/v2.2.0/relicta_Darwin_x86_64.tar.gz"
-      sha256 "ffc27bc02219cfec881faf6f4fd429566e2f61fc0931b42cf3f02876e645f90f"
-    end
-
     on_arm do
       url "https://github.com/relicta-tech/relicta/releases/download/v2.2.0/relicta_Darwin_aarch64.tar.gz"
-      sha256 "0c0cb77e1f9b46a851e10219ab8ae0a7ae686cb645f8373959c5c35210ae1e57"
+      sha256 "2e8f21f906e997ffd653a70d06e317a142a45e31d95c6432bc2ee15e4024cbcb"
+    end
+    on_intel do
+      url "https://github.com/relicta-tech/relicta/releases/download/v2.2.0/relicta_Darwin_x86_64.tar.gz"
+      sha256 "82bb9e098d4c9d242be1300e285cec49df3ab873c1ebda12a627d4236eda97e5"
     end
   end
 
   on_linux do
-    on_intel do
-      url "https://github.com/relicta-tech/relicta/releases/download/v2.2.0/relicta_Linux_x86_64.tar.gz"
-      sha256 "917d6309963a10072742dd9c471ce8341441e7147afdda075230d6ff45bdb706"
-    end
-
     on_arm do
       url "https://github.com/relicta-tech/relicta/releases/download/v2.2.0/relicta_Linux_aarch64.tar.gz"
-      sha256 "7d274006a740b7865f20bbb0beef6b3da971ea2138bdfcc8cc73606389215aff"
+      sha256 "92a95630112b6b0e292aaf18a690a4b9ac2f4427285e8b423a20ff0fda8278a3"
+    end
+    on_intel do
+      url "https://github.com/relicta-tech/relicta/releases/download/v2.2.0/relicta_Linux_x86_64.tar.gz"
+      sha256 "23fda479ed63cbf7c47a37992e1050dfa64ea56902514ab943247c51cb8d6d99"
     end
   end
 
   def install
-    # Find the relicta binary in the extracted directory
-    binary = Dir["**/relicta"].first
-    bin.install binary => "relicta"
+    if OS.mac?
+      if Hardware::CPU.arm?
+        bin.install "relicta_Darwin_aarch64/relicta"
+      else
+        bin.install "relicta_Darwin_x86_64/relicta"
+      end
+    elsif OS.linux?
+      if Hardware::CPU.arm?
+        bin.install "relicta_Linux_aarch64/relicta"
+      else
+        bin.install "relicta_Linux_x86_64/relicta"
+      end
+    end
   end
 
   test do
-    assert_match "relicta v#{version}", shell_output("#{bin}/relicta version")
+    system "#{bin}/relicta", "version"
   end
 end
