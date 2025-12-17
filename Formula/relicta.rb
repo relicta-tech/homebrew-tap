90f2dd1421e8af669809c013912fb8362f82603a
Felix Geelhaar <felix@felixgeelhaar.de>
2025-12-17 15:48:16 +0100

    chore: update relicta to v2.2.0

--- Formula/relicta.rb
+++ Formula/relicta.rb
@@ -4,30 +4,30 @@
 class Relicta < Formula
   desc "AI-powered release management for modern software teams"
   homepage "https://github.com/relicta-tech/relicta"
-  version "2.0.0"
+  version "2.2.0"
   license "MIT"
 
   on_macos do
     on_intel do
-      url "https://github.com/relicta-tech/relicta/releases/download/v2.0.0/relicta_Darwin_x86_64.tar.gz"
-      sha256 "6989a4df22073ee592c88a5e566915d951c9e96435e1d5cb6be4cf9e92a3abbb"
+      url "https://github.com/relicta-tech/relicta/releases/download/v2.2.0/relicta_Darwin_x86_64.tar.gz"
+      sha256 "ffc27bc02219cfec881faf6f4fd429566e2f61fc0931b42cf3f02876e645f90f"
     end
 
     on_arm do
-      url "https://github.com/relicta-tech/relicta/releases/download/v2.0.0/relicta_Darwin_aarch64.tar.gz"
-      sha256 "c4f75612fdcbe000fa5b9fb5bb6b169b4eadd687bcf0dcafd50e5d8ab70b7abb"
+      url "https://github.com/relicta-tech/relicta/releases/download/v2.2.0/relicta_Darwin_aarch64.tar.gz"
+      sha256 "0c0cb77e1f9b46a851e10219ab8ae0a7ae686cb645f8373959c5c35210ae1e57"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/relicta-tech/relicta/releases/download/v2.0.0/relicta_Linux_x86_64.tar.gz"
-      sha256 "5a36a84f72c8220d8410770fbeb414320f985b42a967337979567872372c1a4e"
+      url "https://github.com/relicta-tech/relicta/releases/download/v2.2.0/relicta_Linux_x86_64.tar.gz"
+      sha256 "917d6309963a10072742dd9c471ce8341441e7147afdda075230d6ff45bdb706"
     end
 
     on_arm do
-      url "https://github.com/relicta-tech/relicta/releases/download/v2.0.0/relicta_Linux_aarch64.tar.gz"
-      sha256 "67d4fb40105a2e5bc4a6c24d47879727a7ff879f1e8e70f5b53bb82a2d5f808f"
+      url "https://github.com/relicta-tech/relicta/releases/download/v2.2.0/relicta_Linux_aarch64.tar.gz"
+      sha256 "7d274006a740b7865f20bbb0beef6b3da971ea2138bdfcc8cc73606389215aff"
     end
   end
 
