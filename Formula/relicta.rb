6bec2be2a94606f4db2c786408c9e0554efa9a12
Felix Geelhaar <felix@felixgeelhaar.de>
2025-12-18 17:33:13 +0100

    chore: update relicta to v2.4.1

--- Formula/relicta.rb
+++ Formula/relicta.rb
@@ -5,28 +5,28 @@
 class Relicta < Formula
   desc "AI-powered release management for modern software teams"
   homepage "https://github.com/relicta-tech/relicta"
-  version "2.4.0"
+  version "2.4.1"
   license "MIT"
 
   on_macos do
     on_arm do
       url "https://github.com/relicta-tech/relicta/releases/download/v#{version}/relicta_Darwin_aarch64.tar.gz"
-      sha256 "f1f77b09dd0a973d0776643549fe1f7797ed7ccad6e54f24b427f91b0830c89b"
+      sha256 "6010f752fd21368a106b0043405e08473668ffc307da0c7b050c3be108b85376"
     end
     on_intel do
       url "https://github.com/relicta-tech/relicta/releases/download/v#{version}/relicta_Darwin_x86_64.tar.gz"
-      sha256 "fcadae73db695f3b869a934b3a15fac8ca69c260f819357c7df6cdf83db127f9"
+      sha256 "1cbe603bd9bb2f9754b63dbd7bc02e12b0f93712d71932e2223b497a6db023b4"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/relicta-tech/relicta/releases/download/v#{version}/relicta_Linux_aarch64.tar.gz"
-      sha256 "12bace7a489c5d4febe3eacfb364617f1f14ebf887bd740d99a01d3019f36599"
+      sha256 "c89ff6dcfe5b474c3d4bf3974ce03c0e203e8608f2bacbd98def931335cb2804"
     end
     on_intel do
       url "https://github.com/relicta-tech/relicta/releases/download/v#{version}/relicta_Linux_x86_64.tar.gz"
-      sha256 "b899702c33faa4dcac51def6379c7c646a8b1f2862ef4f800f67eb220ed7354d"
+      sha256 "36f714e6023fc1e8216a416cb58b501b40e92417a55b37cd4aa73e9d9a95e3ac"
     end
   end
 
