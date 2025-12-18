a39545408c33dc2ef88598bbe2fabb14041e245a
Felix Geelhaar <felix@felixgeelhaar.de>
2025-12-18 20:24:06 +0100

    chore(migrate): update formula with v0.1.0 checksums

--- Formula/migrate.rb
+++ Formula/migrate.rb
@@ -11,22 +11,22 @@ class Migrate < Formula
   on_macos do
     on_arm do
       url "https://github.com/relicta-tech/migrate/releases/download/v#{version}/migrate_darwin_arm64.tar.gz"
-      sha256 "PLACEHOLDER_DARWIN_ARM64"
+      sha256 "fde9030fe886a701226b4fa7014b6613d55a4f5b785a4153dafdf9bd324e28e8"
     end
     on_intel do
       url "https://github.com/relicta-tech/migrate/releases/download/v#{version}/migrate_darwin_amd64.tar.gz"
-      sha256 "PLACEHOLDER_DARWIN_AMD64"
+      sha256 "4594d91a9766192a9b3587afece9d0874508f3bf53e554a62ab81f8632518445"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/relicta-tech/migrate/releases/download/v#{version}/migrate_linux_arm64.tar.gz"
-      sha256 "PLACEHOLDER_LINUX_ARM64"
+      sha256 "63597e4d10af750a2ef8eb5fb12245da00fee8ab04690c08578f32c42671b112"
     end
     on_intel do
       url "https://github.com/relicta-tech/migrate/releases/download/v#{version}/migrate_linux_amd64.tar.gz"
-      sha256 "PLACEHOLDER_LINUX_AMD64"
+      sha256 "e0cef4112e7f1ae9d7b652546b770f1de99662a5519cf7a8892a0274cb9f9c4d"
     end
   end
 
