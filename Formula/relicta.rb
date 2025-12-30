4230f75dd0a3bc3f1c6f499b96127cfa06014623
Felix Geelhaar <felix@felixgeelhaar.de>
2025-12-31 00:55:39 +0100

    chore: update formula to v3.0.0

--- Formula/relicta.rb
+++ Formula/relicta.rb
@@ -5,28 +5,28 @@
 class Relicta < Formula
   desc "The governance layer for software change"
   homepage "https://github.com/relicta-tech/relicta"
-  version "2.13.0"
+  version "3.0.0"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/relicta-tech/relicta/releases/download/v#{version}/relicta_Darwin_aarch64.tar.gz"
-      sha256 "cfd65a57068b5e9414700a6e2b3b9b01bc0ee44c31d5eb9ff2271d50ec06efca"
+      url "https://github.com/relicta-tech/relicta/releases/download/v#{version}/relicta_darwin_arm64.tar.gz"
+      sha256 "2a4e1f9697135612b8bed06ea0995b8b1f82dcb5fa6f5cc9f28a1f22d8ae22d0"
     end
     on_intel do
-      url "https://github.com/relicta-tech/relicta/releases/download/v#{version}/relicta_Darwin_x86_64.tar.gz"
-      sha256 "4f02fa7f0adbd98111dffbd4c98f95cf4e949cd80f358f944e1a1e50c6d4fbe5"
+      url "https://github.com/relicta-tech/relicta/releases/download/v#{version}/relicta_darwin_amd64.tar.gz"
+      sha256 "d03602b9a4e0ddd4394b3c030d9d106030db528e804f7d2820ccff6da455a517"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/relicta-tech/relicta/releases/download/v#{version}/relicta_Linux_aarch64.tar.gz"
-      sha256 "d4b50437c0057e417f8fa40f893b2b859f5b931caba9f09276bc0e507f033dd7"
+      url "https://github.com/relicta-tech/relicta/releases/download/v#{version}/relicta_linux_arm64.tar.gz"
+      sha256 "a33afba3021470e3354eb2e3a66772bbd6c18e30e3565e7f7ee359d6f75cd860"
     end
     on_intel do
-      url "https://github.com/relicta-tech/relicta/releases/download/v#{version}/relicta_Linux_x86_64.tar.gz"
-      sha256 "75a7ee8ac7856899f1a5b573415153b651f7cba0cb180816e7f0e4b0e602d0ed"
+      url "https://github.com/relicta-tech/relicta/releases/download/v#{version}/relicta_linux_amd64.tar.gz"
+      sha256 "19d5c35a5cab84a3087dd6dde4c15716ce14e02f30244592ac2b58d2b59d527b"
     end
   end
 
