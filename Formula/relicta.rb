fb36cb408417b45f8e49be215398afd124234ad6
Felix Geelhaar <felix@felixgeelhaar.de>
2025-12-31 11:37:29 +0100

    chore: update relicta formula to v3.0.1

--- Formula/relicta.rb
+++ Formula/relicta.rb
@@ -1,32 +1,32 @@
 # typed: false
 # frozen_string_literal: true
 
-# Homebrew formula for Relicta
+# Relicta Homebrew formula
 class Relicta < Formula
-  desc "The governance layer for software change"
+  desc "AI-powered release governance for modern software teams"
   homepage "https://github.com/relicta-tech/relicta"
-  version "3.0.0"
+  version "3.0.1"
   license "MIT"
 
   on_macos do
-    on_arm do
-      url "https://github.com/relicta-tech/relicta/releases/download/v#{version}/relicta_darwin_arm64.tar.gz"
-      sha256 "2a4e1f9697135612b8bed06ea0995b8b1f82dcb5fa6f5cc9f28a1f22d8ae22d0"
-    end
     on_intel do
-      url "https://github.com/relicta-tech/relicta/releases/download/v#{version}/relicta_darwin_amd64.tar.gz"
-      sha256 "d03602b9a4e0ddd4394b3c030d9d106030db528e804f7d2820ccff6da455a517"
+      url "https://github.com/relicta-tech/relicta/releases/download/v3.0.1/relicta_darwin_amd64.tar.gz"
+      sha256 "5bcf73d88667e1fb8319379c9e611580322fe4a30894017049401bab3bf0b375"
+    end
+    on_arm do
+      url "https://github.com/relicta-tech/relicta/releases/download/v3.0.1/relicta_darwin_arm64.tar.gz"
+      sha256 "777350969c7e3fb7d701fa3c009daf8915b4a2c37bb26160a7db4e1bf2218b63"
     end
   end
 
   on_linux do
-    on_arm do
-      url "https://github.com/relicta-tech/relicta/releases/download/v#{version}/relicta_linux_arm64.tar.gz"
-      sha256 "a33afba3021470e3354eb2e3a66772bbd6c18e30e3565e7f7ee359d6f75cd860"
-    end
     on_intel do
-      url "https://github.com/relicta-tech/relicta/releases/download/v#{version}/relicta_linux_amd64.tar.gz"
-      sha256 "19d5c35a5cab84a3087dd6dde4c15716ce14e02f30244592ac2b58d2b59d527b"
+      url "https://github.com/relicta-tech/relicta/releases/download/v3.0.1/relicta_linux_amd64.tar.gz"
+      sha256 "4581ae38c5aa81056d384064007ecc0f9aa063e742004b4d24515ff3609ca35b"
+    end
+    on_arm do
+      url "https://github.com/relicta-tech/relicta/releases/download/v3.0.1/relicta_linux_arm64.tar.gz"
+      sha256 "62fd27bb244f58170fedf07e66c063c34ca6fed8c6287dd8ee54b040ded26e41"
     end
   end
 
